fc857f96052ae3095d13b62e4c3b7af8edc15f6c
Alexandr Savca <alexandr.savca89@gmail.com>
2026-08-02 16:06:50 +0300

    include: fix Doxygen markup in libpkgsource-yaml.h

--- include/libpkgsource-yaml/libpkgsource-yaml.h
+++ include/libpkgsource-yaml/libpkgsource-yaml.h
@@ -1,7 +1,9 @@
 // SPDX-FileCopyrightText: 2026 Alexandr Savca
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-/*! \file libpkgsource-yaml.h \brief Complete public parser API. */
+/*! \file libpkgsource-yaml.h
+ *  \brief Complete public parser API.
+ */
 #pragma once
 
 #include <libpkgsource-yaml/parser.h>
